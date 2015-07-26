4c4521a42430eb630a7434a045aa2167d22c6e5d
Boino <inigo_azqueta@hotmail.com>
2015-07-26 16:02:45 +0200

    Creation of a function that stores the inverse of a Matrix in the cache, to avoid potential time-consuming operations
    Programming Peer Assignment 2. R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,41 @@
-## [Put comments here that describe what your functions do]
+install.packages("R.cache", dependencies=TRUE)
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { # creating the matrix X
+  inv <- NULL # we state the inverse of the matrix to be empty
+  set <- function(y) { # set a new function within the matrix
+    x <<- y # we assign the value of y to x if it is not previously set
+    inv <<- NULL # we assign the value null to inverse if it is not previously taken
+  }
+  get <- function() x #
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
+
+x <- rbind(c(2, 3), c(2, 2))
+solve(x)
+m = makeCacheMatrix(x)
+m$get()
+
+cacheSolve(m)
+
+
+
+
+
+
+
+
+
